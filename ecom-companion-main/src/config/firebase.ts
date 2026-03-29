70e3774622ff88b8d14721bc5656be4ffdafbb9f
Tushar Kumar <tusharkr756@gmail.com>
2026-03-29 12:49:33 +0530

    env local added

--- ecom-companion-main/src/config/firebase.ts
+++ ecom-companion-main/src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8DQEIz9AUVX951d4qQ3nFG_Kc2lGId7A",
-  authDomain: "ecom-fbf5e.firebaseapp.com",
-  projectId: "ecom-fbf5e",
-  storageBucket: "ecom-fbf5e.firebasestorage.app",
-  messagingSenderId: "729539706532",
-  appId: "1:729539706532:web:6e4f9eacad05e61b79b031",
-  measurementId: "G-GWH3N2ET9C"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
